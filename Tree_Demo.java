3d4b4857809c59bbd969d1a65c9c59fc88303b51
stefan-lin <sir.stefee@gmail.com>
2015-09-15 00:03:27 -0700

    Need more works on this demo

--- Tree_Demo.java
+++ Tree_Demo.java
@@ -3,71 +3,85 @@
  */
 public class Tree_Demo {
   public static void main(String[] args) {
-	  
-	//test null case
-	BST<Integer> binary = new BST();
-	binary.insert_node(null);
-	binary.print();
-	System.out.println("Empty BST");
-	System.out.println();
-	
-	//print a 5 lvl BST
-	BST<Integer> binary2 = new BST();
-	binary2.insert_node(50);
-	binary2.insert_node(45);
-	binary2.insert_node(65);
-	binary2.insert_node(40);
-	binary2.insert_node(48);
-	binary2.insert_node(60);
-	binary2.insert_node(70);
-	binary2.insert_node(30);
-	binary2.insert_node(35);
-	binary2.insert_node(47);
-	binary2.insert_node(53);
-	binary2.insert_node(68);
-	binary2.insert_node(75);	
-	binary2.print();
-	System.out.println("Successful printing of 5 lvl tree");
-	System.out.println();
-	
-	//testing Delete func of BST
-	BST<Integer> binary3 = new BST();
-	binary3.insert_node(50);
-	binary3.insert_node(45);
-	binary3.insert_node(65);
-	binary3.insert_node(40);
-	binary3.insert_node(48);
-	binary3.insert_node(60);
-	binary3.insert_node(70);
-	binary3.insert_node(30);
-	binary3.insert_node(35);
-	binary3.insert_node(47);
-	binary3.insert_node(53);
-	binary3.insert_node(68);
-	binary3.insert_node(75);
-	//delete starts here
-	/*binary3.delete_node(50);
-	binary3.delete_node(40);//gives null pt exception, delete_node line 245/228
-	binary3.delete_node(null);
-	binary3.delete_node(null);
-	binary3.delete_node(null);
-	binary3.delete_node(null);
-	binary3.delete_node(null);
-	binary3.delete_node(null);
-	binary3.delete_node(null);
-	binary3.delete_node(null);*/
-	
-	AVL_Tree av1 = new AVL_Tree();
-	
-	av1.insert_node(null); //print nothing
-	av1.print();
-	System.out.println("Empty AVL Tree");
-	System.out.println();
-	
-	AVL_Tree av2 = new AVL_Tree();
-	av2.delete_node(40);
-	
-   // AVL_Tree __avl = new AVL_Tree();
+
+    //test null case
+    BST<Integer> binary = new BST();
+    binary.insert_node(null);
+    binary.print();
+    System.out.println("Empty BST");
+    System.out.println();
+
+    //print a 5 lvl BST
+    BST<Integer> binary2 = new BST();
+    binary2.insert_node(50);
+    binary2.insert_node(45);
+    binary2.insert_node(65);
+    binary2.insert_node(40);
+    binary2.insert_node(48);
+    binary2.insert_node(60);
+    binary2.insert_node(70);
+    binary2.insert_node(30);
+    binary2.insert_node(35);
+    binary2.insert_node(47);
+    binary2.insert_node(53);
+    binary2.insert_node(68);
+    binary2.insert_node(75);
+    binary2.print();
+    System.out.println("Successful printing of 5 lvl tree");
+    System.out.println();
+
+    //testing Delete func of BST
+    BST<Integer> binary3 = new BST();
+    binary3.insert_node(50);
+    binary3.insert_node(45);
+    binary3.insert_node(65);
+    binary3.insert_node(40);
+    binary3.insert_node(48);
+    binary3.insert_node(60);
+    binary3.insert_node(70);
+    binary3.insert_node(30);
+    binary3.insert_node(35);
+    binary3.insert_node(47);
+    binary3.insert_node(53);
+    binary3.insert_node(68);
+    binary3.insert_node(75);
+
+    // TESTING
+    //binary3.print();
+
+    //delete starts here
+	  binary3.delete_node(50);
+
+    // TESTING
+    //binary3.print();
+
+	  binary3.delete_node(40);//gives null pt exception, delete_node line 245/228
+
+    // TESTING
+    //binary3.print();
+
+	  binary3.delete_node(null);
+	  binary3.delete_node(null);
+	  binary3.delete_node(null);
+	  binary3.delete_node(null);
+	  binary3.delete_node(null);
+	  binary3.delete_node(null);
+	  binary3.delete_node(null);
+	  binary3.delete_node(null);
+
+
+    AVL_Tree av1 = new AVL_Tree();
+
+    av1.insert_node(null); //print nothing
+    av1.print();
+    System.out.println("Empty AVL Tree");
+    System.out.println();
+
+    AVL_Tree av2 = new AVL_Tree();
+    av2.delete_node(40);
+    
+    //////////////////////////////////////////
+    // AVL_Tree __avl = new AVL_Tree();
 
     //__avl.insert_node(50);
     //__avl.insert_node(35);
